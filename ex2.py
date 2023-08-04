de4f6e4655928e5afa4833d3d8f733917a5fe65f
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-04 20:50:01 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#check the given input is positive or negative
+number=int(input("Enter the number to be checked:"))
+if number>0:
+   print("yes it is an positive number")
+else:
+   print("no it is not a positive number")
