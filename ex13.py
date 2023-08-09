d5d0928ae4c9e22b380c80cd4267b817202e97e8
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-09 15:37:52 +0530

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,10 @@
+
+#token buy in a museum
+h=int(input("enter the height: "))
+if h>3:
+      print("buy token")
+elif h==3:
+    print("ask the ticket checker")
+else:
+    print("no token required")
+print("buy token properly")
