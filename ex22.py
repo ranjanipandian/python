3cdac0ecf3a7d282e850c064cbdbf6b40d3ccfde
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-09 15:44:48 +0530

    Add files via upload

---
+++ ex22.py
@@ -0,0 +1,35 @@
+#random module
+
+import random
+
+#randint 
+a=random.randint(1,7)
+print(a)
+
+
+#randrange
+a=random.randrange(1,7)
+print(a)
+
+#random
+a=random.random
+print(a)
+
+#uniform
+a=random.uniform(1,7)
+print(a)
+
+
+
+#choice
+list=[1,2,3,4,5,6,7,8]
+a=random.choice(list)
+print(a)
+
+#shuffle
+list=[1,2,3,4,5,6,7,8]
+random.shuffle(list)
+print(list)
+
+
+
