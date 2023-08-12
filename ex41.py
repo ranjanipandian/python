9d714908453693354d68431bdcefdcb833aa86ba
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-12 16:08:17 +0530

    Add files via upload

---
+++ ex41.py
@@ -0,0 +1,9 @@
+#count the length of the elements in the given list without using len function
+
+
+h=input("Enter the heights:")
+height_list=h.split()
+c=0
+for i in height_list:
+        c=c+1
+print(c)
