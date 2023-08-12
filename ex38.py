571a18a99125219effaca4040b2ee96c4dd2155b
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-12 16:07:05 +0530

    Add files via upload

---
+++ ex38.py
@@ -0,0 +1,34 @@
+#for loop in python
+
+
+# A loop is thing in which repeat the same process again and again until the condition is satisfied
+
+#for loop for a single list
+name=['ranjani']
+for i in name:
+    print(i)
+
+print("\n")
+
+
+#for loop for multiple list
+name=['ranjani','anu','suresh','amutha','pandian',]
+for i in name:
+    print(i)
+    if i=='ranjani':
+        print("hey !! how r u")
+        print("\n")
+
+
+
+#to print a list in for loop
+n=[1,2,3,4,5,6,7,8,9]
+y=[]
+for i in n:
+    s= i**2
+    y.append(s)
+print("The list of the Square List is:",y)
+
+
+
+
