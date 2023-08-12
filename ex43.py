fc79f1b95a244c6cf228e349b57c470756ae0b40
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-12 16:08:49 +0530

    Add files via upload

---
+++ ex43.py
@@ -0,0 +1,21 @@
+#range() function
+
+#the syntax is:
+#range(start,stop,step_size)
+#default: start is o and step_size is 1 but stop is compulsory
+
+
+
+#example 1
+a=input()
+for i in range(0,5,1):
+    print(i)
+print("\n")
+#example 2
+a=range(0,3,2)
+for i in a:
+    print(i)
+    
+
+
+
