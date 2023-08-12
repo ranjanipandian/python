602b22636530d509bde63c96806ef5978219d257
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-12 17:58:44 +0530

    Add files via upload

---
+++ ex46.py
@@ -0,0 +1,23 @@
+#while loop
+
+#1:
+c=1
+while c<=5:
+    print(c)
+    c+=1
+print("You are out of the Loop")
+
+#2:
+c=1
+while c<=5:
+    print(c)
+    c+=1
+    break;
+else:
+  printf("Y R U here!")
+  print("You are out of the Loop")
+
+#3:
+c=1
+while c<=5: print(c);c+=1
+print("You are out of the Loop")
