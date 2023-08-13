69ecddf078915cffaf58b6ce299fa0a13fdedb69
Ranjanipandian <113160519+ranjanipandian@users.noreply.github.com>
2023-08-13 10:15:06 +0530

    Add files via upload

---
+++ word_file.py
@@ -0,0 +1,8 @@
+words=["dance","high","our","spouse","able","danger","hill","out","spring","about","dark","him","outside","square","above","daughter","himself","over","stairs","accident","day",
+       "hint","own","stairway","acid","decide","his","oxygen","stand","across","decided","history","page","stars","act","decimal","hold","paint","start","add","deep","hole",
+       "pair","state","admission","delivery","home","pants","statement","after","deposit","horse","paragraph","step","again","describe","hospital","parents","stick","against",
+       "desert","hot","park","still","age","design","hotel","part","stone","ago","desk","hours","party","stood","both","example","local","red","touch","bottom","exercise",
+       "long","refund","tow","bowl","exists","look","region","toward","box","experiment","lost","agree","destination","house","passed","stop","remain","town","boy","explain",
+       "lot","remember","train","explosives","aide","developed","how","passengers","store","air","diary","storm","alarm","dictionary","huge","past","story","human","pattern",
+       "stove","almost","hundred","pay","straight","alone","died","husband","payment","strange","already","difference","pedestrians","street","different","pen"	,"although",
+       "digital","ice","pencil","students","always","diner","idea","people","study","dinner","subject","ambulance","direct","important","period","subtract"]
